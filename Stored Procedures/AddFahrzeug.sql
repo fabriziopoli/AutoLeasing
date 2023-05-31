2160b1ca1e7a9b02f0d28dbfe91debe4dfae3bab
Thierry Lötscher <thierry.loetscher@komaxgroup.com>
2023-05-31 08:59:00 +0200

    added new store procedure

--- Stored Procedures/AddFahrzeug.sql
+++ Stored Procedures/AddFahrzeug.sql
@@ -8,3 +8,14 @@ BEGIN
     INSERT INTO Fahrzeug (Marke, Modell, Baujahr, Preis)
     VALUES (@Marke, @Modell, @Baujahr, @Preis)
 END
+
+--Abfüllen von Daten
+EXEC AddFahrzeug @Marke = 'Ford Mustang', @Modell = '289', @Baujahr = 1965, @Preis = 77423;
+
+--Test zum Procedure AddFahrzeug
+SELECT * FROM Fahrzeug WHERE Marke = 'Ford Mustang' AND Modell = '289';
+
+
+
+
+
